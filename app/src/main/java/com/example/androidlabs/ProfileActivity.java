4ed8454711ba271631f94c63dbcdec0a5033a2f2
DikshitDikshit <bravedaksh1234@gmail.com>
2020-03-09 17:03:30 -0400

    mobile

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -48,6 +49,17 @@ public class ProfileActivity extends AppCompatActivity {
             Intent chatIntent = new Intent(ProfileActivity.this, ChatRoomActivity.class);
             startActivity(chatIntent);
         });
+        goToWeatherBtn = findViewById(R.id.buttongotoweather);
+        goToWeatherBtn.setOnClickListener(c -> {
+            seeWeather();
+        });
+    }
+    Button goToWeatherBtn;
+    public void seeWeather(){
+
+        Intent goToMenuPage = new Intent(ProfileActivity.this, WeatherForecast.class);
+
+        startActivityForResult(goToMenuPage, 234);
 
     }
 
